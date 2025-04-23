208be2ea23feb67638fbeca25e91aaf3fe5df067
Masaya Nakakuki <masa8y88@gmail.com>
2025-04-23 15:49:44 +0900

    3章拡張関数

--- HelloCompose/app/src/main/java/com/example/myhellocompose/MainActivity.kt
+++ HelloCompose/app/src/main/java/com/example/myhellocompose/MainActivity.kt
@@ -1,57 +1,24 @@
 package com.example.myhellocompose
 
-import android.graphics.drawable.Icon
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.annotation.DrawableRes
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.ColumnScope
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.icons.Icons
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.mutableStateMapOf
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.myhellocompose.ui.theme.MyHelloComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -70,40 +37,10 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-private fun AlertDialogSample(
-    modifier: Modifier = Modifier
-) {
-    AlertDialog(
-        onDismissRequest = { Log.d("onDismiss", "onDismissRequest is selected") },
-        confirmButton = {
-            TextButton(
-                onClick = { Log.d("confirm", "OK is selected") }
-            ) {
-                Text(text = "OK")
-            }
-        },
-        dismissButton = {
-            TextButton(
-                onClick = { Log.d("confirm", "Cacnel is selected") }
-            ) {
-                Text(text = "Cancel")
-            }
-        },
-        icon = {
-            Icon(
-                painter = painterResource(id = R.drawable.info),
-                contentDescription = null,
-                modifier = Modifier.size(width = 24.dp, height = 24.dp)
-            )
-        },
-        title = { Text(text = "Sample Dialog") },
-        text = { Text(text = "This is a compose sample dialog") },
-        containerColor = Color.White,
-        iconContentColor = Color.LightGray,
-        titleContentColor = Color.Black,
-        textContentColor = Color.Gray
-    )
+private fun String.ExtensionString() {
+    if (isNotBlank()) {
+        Log.d("StringCount", length.toString())
+    }
 }
 
 @Preview(showBackground = true)
