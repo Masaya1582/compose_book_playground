20c4796dad47c69b4b568f9fabab44af66f00666
Masaya Nakakuki <masa8y88@gmail.com>
2025-04-25 16:20:39 +0900

    DatePickerとTimePicker

--- HelloCompose/app/src/main/java/com/example/myhellocompose/MainActivity.kt
+++ HelloCompose/app/src/main/java/com/example/myhellocompose/MainActivity.kt
@@ -1,24 +1,34 @@
 package com.example.myhellocompose
 
+import android.app.TimePickerDialog
+import android.icu.util.Calendar
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.navigation.NavType
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
-import androidx.navigation.navArgument
-import com.example.myhellocompose.data.AppDestinations
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.myhellocompose.ui.theme.MyHelloComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -27,60 +37,106 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MyHelloComposeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    PokemonApp(modifier = Modifier.padding(innerPadding))
-                }
+               SampleDateAndTimePickerView()
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview(showBackground = false)
 @Composable
-fun PokemonApp(modifier: Modifier = Modifier) {
-    val navController = rememberNavController()
+private fun SampleDateAndTimePickerView() {
+    Scaffold(
+        topBar = {
+            TopAppBar(title = { Text("Date & Time Picker Example") })
+        }
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier
+                .padding(paddingValues)
+                .fillMaxSize()
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            DateTimePickerScreen()
+        }
+    }
+}
 
-    NavHost(
-        navController = navController,
-        startDestination = AppDestinations.POKEMON_LIST_ROUTE
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun DateTimePickerScreen() {
+    val calendar = Calendar.getInstance()
+    val year = calendar.get(Calendar.YEAR)
+    val month = calendar.get(Calendar.MONTH)
+    val day = calendar.get(Calendar.DAY_OF_MONTH)
+    var selectedDate by remember { mutableStateOf("$day/${month + 1}/$year") }
+
+    val hour = calendar.get(Calendar.HOUR_OF_DAY)
+    val minute = calendar.get(Calendar.MINUTE)
+    var selectedTime by remember { mutableStateOf(String.format("%02d:%02d", hour, minute)) }
+
+    var isShowDatePickerDialog by remember { mutableStateOf(false) }
+    var isShowTimePickerDialog by remember { mutableStateOf(false) }
+
+    Text(
+        text = "Selected Date: $selectedDate",
+        style = MaterialTheme.typography.headlineMedium
+    )
+    Button(
+        modifier = Modifier.fillMaxWidth(),
+        onClick = {
+            isShowDatePickerDialog = true
+        }
     ) {
-        composable(AppDestinations.POKEMON_LIST_ROUTE) {
-            PokemonListScreen(
-                onPokemonClick = { name ->
-                    navController.navigate("${AppDestinations.POKEMON_DETAIL_ROUTE}/$name")
-                }
-            )
-        }
-
-        composable(
-            route = AppDestinations.POKEMON_DETAIL_ROUTE_WITH_ARGS,
-            arguments = listOf(navArgument(AppDestinations.POKEMON_NAME_KEY) {
-                type = NavType.StringType
-            })
-        ) { backStackEntry ->
-            val name = backStackEntry.arguments?.getString(AppDestinations.POKEMON_NAME_KEY)
-            PokemonDetailScreen(pokemonName = name ?: "不明")
-        }
+        Text("Pick Date")
     }
-}
+    Spacer(modifier = Modifier.height(24.dp))
+    Text(
+        text = "Selected Time: $selectedTime",
+        style = MaterialTheme.typography.headlineMedium,
+        modifier = Modifier.padding(bottom = 8.dp)
+    )
 
-@Composable
-fun PokemonListScreen(onPokemonClick: (String) -> Unit) {
-    val pokemons = listOf("Pikachu", "Charmander", "Squirtle")
-
-    Column {
-        Text(text = "ポケモン一覧", style = MaterialTheme.typography.headlineMedium)
-        pokemons.forEach { name ->
-            Button(onClick = { onPokemonClick(name) }) {
-                Text(text = name)
-            }
+    // Button to show TimePicker
+    Button(
+        modifier = Modifier.fillMaxWidth(),
+        onClick = {
+            isShowTimePickerDialog = true
         }
+    ) {
+        Text("Pick Time")
     }
-}
 
-@Composable
-fun PokemonDetailScreen(pokemonName: String) {
-    Column {
-        Text(text = "ポケモン詳細", style = MaterialTheme.typography.headlineMedium)
-        Text(text = "選ばれたポケモン: $pokemonName", style = MaterialTheme.typography.bodyLarge)
+    if (isShowDatePickerDialog) {
+        val context = LocalContext.current
+        android.app.DatePickerDialog(
+            context,
+            { _, selectedYear, selectedMonth, selectedDay ->
+                selectedDate =
+                    "$selectedDay/${selectedMonth + 1}/$selectedYear"
+                isShowDatePickerDialog = false
+            },
+            year,
+            month,
+            day
+        ).show()
+    }
+
+    if (isShowTimePickerDialog) {
+        val context = LocalContext.current
+        TimePickerDialog(
+            context,
+            { _, selectedHour, selectedMinute ->
+                // Update the selected time state when a time is picked
+                selectedTime = String.format("%02d:%02d", selectedHour, selectedMinute)
+                isShowTimePickerDialog = false // Hide the dialog
+            },
+            hour,
+            minute,
+            true // is24HourView
+        ).show() // Show the dialog immediately when state is true
     }
 }
